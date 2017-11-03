4c04099b2ac37e153590231ee866c35f8302f35d
AlexFin <alexey.finohenov@gmail.com>
2017-11-03 22:34:44 +0200

    adaptiv menu

--- scrypt.js
+++ scrypt.js
@@ -1,21 +1,6 @@
-var d = document;
-var wrap = d.querySelector('.wrap');
-var items = d.querySelector('.items');
-var itemCount = d.querySelectorAll('.item').length;
-var scroller = d.querySelector('.scroller');
-var pos = 0;
-var transform = Modernizr.prefixed('transform');
+$('.mobile-tab').hide();
 
-function setTransform() {
- items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)';
-}
 
-function prev() {
- pos = Math.max(pos - 1, 0);
- setTransform();
-}
-function next() {
- pos = Math.min(pos + 1 , itemCount - 1);
- setTransform();
-}
-window.addEventListener('resize', setTransform);﻿
+$('#burg').on('click',function () {
+	$('.mobile-tab').slideToggle(1000);
+})
